c67b6ab1cbf332e37a06441f64584ec9027c6ae4
Asaf Gardin <asafg@ai21.com>
2024-02-21 13:43:28 +0200

    feat: contextual answers

--- libs/partners/ai21/langchain_ai21/__init__.py
+++ libs/partners/ai21/langchain_ai21/__init__.py
@@ -1,17 +1,11 @@
 from langchain_ai21.chat_models import ChatAI21
 from langchain_ai21.embeddings import AI21Embeddings
 from langchain_ai21.llms import AI21LLM
-from langchain_ai21.contextual_answers import (
-    AI21ContextualAnswers,
-    ContextualAnswerBody,
-    AI21ContextualAnswersLLM,
-)
+from langchain_ai21.contextual_answers import AI21ContextualAnswers
 
 __all__ = [
     "AI21LLM",
     "ChatAI21",
     "AI21Embeddings",
     "AI21ContextualAnswers",
-    "ContextualAnswerBody",
-    "AI21ContextualAnswersLLM",
 ]
--- libs/partners/ai21/langchain_ai21/contextual_answers.py
+++ libs/partners/ai21/langchain_ai21/contextual_answers.py
@@ -1,38 +1,26 @@
-from abc import ABC
-from typing import Optional, Union, Any, Type, Tuple, Sequence, TypeAlias, List
+from typing import (
+    Optional,
+    Union,
+    Any,
+    Type,
+    TypeAlias,
+    TypedDict,
+)
 
 from langchain_ai21.ai21_base import AI21Base
-from langchain_core.callbacks import Callbacks
 from langchain_core.documents import Document
-from langchain_core.language_models.base import (
-    LanguageModelOutput,
-    BaseLanguageModel,
-)
-from langchain_core.load import Serializable
-from langchain_core.messages import BaseMessage
-from langchain_core.outputs import LLMResult
-from langchain_core.prompt_values import StringPromptValue, PromptValue
+from langchain_core.prompt_values import StringPromptValue
 from langchain_core.runnables import RunnableConfig, RunnableSerializable
-from langchain_core.runnables.utils import Input, Output
 
 TSMModelInput = Union[StringPromptValue, str]
 
 
-class ContextualAnswerBody(Serializable):
+class ContextualAnswerInput(TypedDict):
     context: str
     question: str
 
 
-ContextualAnswerStrDocType = Tuple[str, Sequence[Document]]
-ContextualAnswerInputType = Union[
-    ContextualAnswerBody,
-    ContextualAnswerStrDocType,
-]
-
-
-class AI21ContextualAnswersLLM(
-    RunnableSerializable[ContextualAnswerInputType, str], AI21Base
-):
+class AI21ContextualAnswers(RunnableSerializable[ContextualAnswerInput, str], AI21Base):
     class Config:
         """Configuration for this pydantic object."""
 
@@ -41,89 +29,41 @@ class AI21ContextualAnswersLLM(
     @property
     def InputType(self) -> TypeAlias:
         """Get the input type for this runnable."""
-        return ContextualAnswerInputType
+        return ContextualAnswerInput
 
     @property
     def OutputType(self) -> Type[str]:
         """Get the input type for this runnable."""
         return str
 
-    def _convert_input(
-        self, input: ContextualAnswerInputType
-    ) -> ContextualAnswerBody:
-        if isinstance(input, ContextualAnswerBody):
+    def _convert_input(self, input: ContextualAnswerInput) -> ContextualAnswerInput:
+        # if input.get("context") is None:
+        #     raise ValueError("Input must contain a 'context' field")
+        context_input = input["context"]
+
+        if isinstance(context_input, list):
+            docs = [
+                item.page_content if isinstance(item, Document) else item
+                for item in context_input
+            ]
+            return {"context": "\n".join(docs), "question": input["question"]}
+
+        if isinstance(context_input, str):
             return input
 
-        if isinstance(input, tuple):
-            if isinstance(input[1], Document):
-                docs = "\n".join([document.page_content for document in input[1]])
-                return ContextualAnswerBody(context=docs, question=input[0])
-
-        raise ValueError(f"Invalid input type {type(input)}")
+        raise ValueError(
+            f"Expected input to be a list of strings or Documents. Received {type(input)}"
+        )
 
     def invoke(
         self,
-        input: ContextualAnswerInputType,
+        input: ContextualAnswerInput,
         config: Optional[RunnableConfig] = None,
         **kwargs: Any,
-    ) -> LanguageModelOutput:
+    ) -> str:
         converted_input = self._convert_input(input)
         response = self.client.answer.create(
-            context=converted_input.context, question=converted_input.question
+            context=converted_input["context"], question=converted_input["question"]
         )
 
-        if response.answer_in_context:
-            return response.answer
-
-        return "Answer Not in Context"
-
-
-class AI21ContextualAnswer(BaseLanguageModel[ContextualAnswerInputType], AI21Base):
-    def generate_prompt(
-        self,
-        prompts: List[PromptValue],
-        stop: Optional[List[str]] = None,
-        callbacks: Callbacks = None,
-        **kwargs: Any,
-    ) -> LLMResult:
-        pass
-
-    async def agenerate_prompt(
-        self,
-        prompts: List[PromptValue],
-        stop: Optional[List[str]] = None,
-        callbacks: Callbacks = None,
-        **kwargs: Any,
-    ) -> LLMResult:
-        pass
-
-    def invoke(self, input: Input, config: Optional[RunnableConfig] = None) -> Output:
-        pass
-
-    def predict(
-        self, text: str, *, stop: Optional[Sequence[str]] = None, **kwargs: Any
-    ) -> str:
-        raise DeprecationWarning("Use `generate_prompt` instead")
-
-    def predict_messages(
-        self,
-        messages: List[BaseMessage],
-        *,
-        stop: Optional[Sequence[str]] = None,
-        **kwargs: Any,
-    ) -> BaseMessage:
-        raise DeprecationWarning("Use `generate_prompt` instead")
-
-    async def apredict(
-        self, text: str, *, stop: Optional[Sequence[str]] = None, **kwargs: Any
-    ) -> str:
-        raise DeprecationWarning("Use `generate_prompt` instead")
-
-    async def apredict_messages(
-        self,
-        messages: List[BaseMessage],
-        *,
-        stop: Optional[Sequence[str]] = None,
-        **kwargs: Any,
-    ) -> BaseMessage:
-        raise DeprecationWarning("Use `generate_prompt` instead")
+        return response.answer
